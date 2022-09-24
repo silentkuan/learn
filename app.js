158532da0181d10924a0e2da973db2a0f657fa73
silentkuan <jiunying2000kjy@gmail.com>
2022-09-24 14:48:36 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+require('dotenv').config();
+const express= require("express");
+const bodyParser= require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const encrypt= require("mongoose-encryption");
+
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect(
+    "mongodb+srv://silent:silent@cluster1.yxqr40k.mongodb.net/userDB?retryWrites=true&w=majority",
+    { useNewUrlParser: true },function(err){
+        if(!err){
+            console.log("connect mongodb successfully");
+        }else{
+            console.log(err);
+        }
+    }
+  );
+
+  const userSchema= new mongoose.Schema({
+    email:String,
+    password:String
+  });
+
+  const secret="thisisaboutlittlesecret.";
+  userSchema.plugin(encrypt,{secret:secret, encryptedFields:['password']});
+
+  const User=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+   const newUser= new User({
+    email: req.body.username,
+    password: req.body.password
+   });
+
+   newUser.save(function(err){
+    if(!err){
+        res.render("secrets");
+    }else{
+        console.log(err);
+    }
+   });
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    const username= req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password===password){
+                    res.render("secrets");
+                }else{
+                    console.log("Enter Wrong Password!");
+                }
+            }
+        }
+    })
+});
+
+app.listen(3000, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", 3000);
+})
